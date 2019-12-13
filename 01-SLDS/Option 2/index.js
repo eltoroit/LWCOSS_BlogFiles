19c788181dbc4e93c3e03b1ba75f0a6a88783a37
Andres Perez <andresperezf@yahoo.com>
2019-12-13 04:58:57 -0500

    Added Solution (start project)

---
+++ 01-SLDS/Option 2/index.js
@@ -0,0 +1,5 @@
+import '@lwc/synthetic-shadow';
+import { buildCustomElementConstructor } from 'lwc';
+import MyApp from 'my/app';
+
+customElements.define('my-app', buildCustomElementConstructor(MyApp));
